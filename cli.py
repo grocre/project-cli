1c55fbedf573e611832b07dc35546d90663587cb
grocre <grocre@gmail.com>
2021-09-27 22:42:33 -0300

    some new commands

--- cli.py
+++ cli.py
@@ -1,12 +1,21 @@
+import os
 from argparse import ArgumentParser
 
 
+parser = ArgumentParser()
 
-parse = ArgumentParser()
+parser.add_argument('projectName', help="Digite o nome do projeto")
 
-parse.add_argument('projectName', help="Digite o nome do projeto")
-
-args = parse.parse_args()
+parser.add_argument("--path", default="./".format(), help="Digite o caminho para a pasta que deseja que seja criada") # resolver isso
 
 
-print("Projeto {} criado".format(args.projectName))
+args = parser.parse_args()
+
+if args.path == "./": 
+    path = "{}{}".format(args.path, args.projectName)
+else: 
+    path = args.path
+
+os.mkdir(path)
+
+print("Projeto {} criado em {}".format(args.projectName, path))
